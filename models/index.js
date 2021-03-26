6ec2aca710135bbe3d20922cc66f35933a4eb5a1
Gina Vargas <gina.vargas89@outlook.com>
2021-03-26 15:05:30 -0700

    models files completed

--- models/index.js
+++ models/index.js
@@ -1,5 +1,9 @@
 const User = require('./User');
 const Profile = require('./Profile');
+const Reserve = require('./Reserve');
+const Reservations = require('./Reservations');
+const Waitlist = require('./Waitlist');
+const ReservationWaitlist = require('./ReservationWaitlist');
 
 User.hasMany(Profile, {
   foreignKey: 'user_id',
@@ -10,4 +14,20 @@ Profile.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Profile };
+Reserve.belongsToMany(User, {
+  through: {
+    model: Reservations,
+    unique: false
+  }
+})
+
+Waitlist.belongsToMany(User, {
+  through: {
+    model: ReservationWaitlist,
+    unique: false
+  }
+})
+
+
+
+module.exports = { User, Profile, Reserve, Reservations, Waitlist, ReservationWaitlist };
