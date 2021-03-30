c8b7932869994ab57b177677084f638cabacef23
sharon <martinezsharonr@gmail.com>
2021-03-29 23:07:01 -0800

    handlebars

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -48,7 +48,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout', async (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
